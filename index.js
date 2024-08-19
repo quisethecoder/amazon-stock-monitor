b8a108dcb4ce820e477f63c03e793257e42be5de
Marquise <marquise_dawkins@aol.com>
2024-08-19 18:42:25 -0400

    add headers

--- index.js
+++ index.js
@@ -7,12 +7,21 @@ const productLink = "https://www.amazon.com/ASUS-C424MA-AS48F-Chromebook-NanoEdg
 
 async function Monitor(){
     var myHeaders = {
-        //Have to add connection here 
+        "connection" : "keep-alive",
         "sec-ch-ua": `"Not)A;Brand";v="99", "Google Chrome";v="127", "Chromium";v="127"`,
         "sec-ch-ua-mobile": "?0",
         "upgrade-insecure-requests": 1,
         "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36",
-
+        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+        "sec-fetch-site": "same-origin",
+        "sec-fetch-mode": "navigate",
+        "sec-fetch-user": "?1",
+        "sec-fetch-dest": "document",
+        "accept-encoding": "gzip, deflate, br, zstd",
+        "accept-language": "en-US,en;q=0.9",
+        "rtt": 100,
+        "ect": "4g",
+        "downlink": 10
     }
 
     const response = await got(productLink,{
