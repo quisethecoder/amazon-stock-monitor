8285affe356d20afc43ff650f929f443120222a6
Marquise <marquise_dawkins@aol.com>
2024-08-19 19:52:48 -0400

    prompt const fix

--- prompt.js
+++ prompt.js
@@ -1,1 +1,12 @@
-const prompt = require("prompt-sync");
+const prompt = require("prompt-sync")();
+
+
+var gender = prompt("Are you a male or female? (M - male, F - female): ");
+
+if(gender == "M" || gender == "F"){
+    console.log(gender);
+}else{
+    console.log("Try Again");
+}
+
+
