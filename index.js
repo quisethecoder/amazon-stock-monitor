8b41a02d6404b54dbf703e8d139e26ece83c2c5c
Marquise <marquise_dawkins@aol.com>
2024-08-19 19:47:23 -0400

    add prompt const

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const got = require("got");
 const HTMLParser = require("node-html-parser");
+const prompt = require("prompt-sync");
 
 
 const productLink = "https://www.amazon.com/ASUS-C424MA-AS48F-Chromebook-NanoEdge-Processor/dp/B0BSP51S36/ref=sr_1_1_sspa?crid=39W5M7N5H9GW8&dib=eyJ2IjoiMSJ9.M3Oz8YWMw4uvmfaIiAaJD-zaCLgwMg45vPdBvqRrlHaqx1i_sI6BSx-M8AEE3GFxFpYB9aKq83LG0REO2DegUuDvzwDZb3SzjZuLhlsVad-EVZf3JyxLO8mcRCkHHlB3gSXYgKqYjU1GuqxD5FVpUZRxCjn2j58dT75Qn69s5wMaUaHIFeOASjeeq8Xdu1AiIDqxogSRtF2Tl90UTdnu05Ngvo3-2_gPqhst4rRGIEQ.ZQWLf-fY9ouM2BK5NA3koLzWGnd_FrmCZLGLtII7o_I&dib_tag=se&keywords=chromebook&qid=1723919179&sprefix=chrome%2Caps%2C482&sr=8-1-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&psc=1";
