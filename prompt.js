7fb9e711d530504256fee05349dca5649afcefd2
Marquise <marquise_dawkins@aol.com>
2024-08-19 20:00:04 -0400

    add to uppercase

--- prompt.js
+++ prompt.js
@@ -1,7 +1,7 @@
 const prompt = require("prompt-sync")();
 
 
-var gender = prompt("Are you a male or female? (M - male, F - female): ");
+var gender = prompt("Are you a male or female? (M - male, F - female): ").toUpperCase();
 
 if(gender == "M" || gender == "F"){
     console.log(gender);
