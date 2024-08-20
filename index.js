c07d662bb78ab7c701e56c6d83ae03934e8a95bb
Marquise <marquise_dawkins@aol.com>
2024-08-20 18:04:34 -0400

    check multiple stocks

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const got = require("got");
 const HTMLParser = require("node-html-parser");
 const prompt = require("prompt-sync")();
 
-//https://www.amazon.com/ASUS-C424MA-AS48F-Chromebook-NanoEdge-Processor/dp/B0BSP51S36/ref=sr_1_1_sspa?crid=39W5M7N5H9GW8&dib=eyJ2IjoiMSJ9.M3
+//https://www.amazon.com/ASUS-C424MA-AS48F-Chromebook-NanoEdge-Processor/dp/B0BSP51S36/ref=sr_1_1_sspa?crid=39W5M7N5H9GW8&dib=eyJ2IjoiMSJ9.M3, https://www.amazon.com/Skytech-Gaming-Nebula-PC-Desktop/dp/B0C9PNZJCF/?
 // const productLink = "https://www.amazon.com/ASUS-C424MA-AS48F-Chromebook-NanoEdge-Processor/dp/B0BSP51S36/ref=sr_1_1_sspa?crid=39W5M7N5H9GW8&dib=eyJ2IjoiMSJ9.M3Oz8YWMw4uvmfaIiAaJD-zaCLgwMg45vPdBvqRrlHaqx1i_sI6BSx-M8AEE3GFxFpYB9aKq83LG0REO2DegUuDvzwDZb3SzjZuLhlsVad-EVZf3JyxLO8mcRCkHHlB3gSXYgKqYjU1GuqxD5FVpUZRxCjn2j58dT75Qn69s5wMaUaHIFeOASjeeq8Xdu1AiIDqxogSRtF2Tl90UTdnu05Ngvo3-2_gPqhst4rRGIEQ.ZQWLf-fY9ouM2BK5NA3koLzWGnd_FrmCZLGLtII7o_I&dib_tag=se&keywords=chromebook&qid=1723919179&sprefix=chrome%2Caps%2C482&sr=8-1-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&psc=1";
 
 async function Monitor(productLink){
@@ -50,14 +50,25 @@ await new Promise(r => setTimeout(r, 300));
 }
 
 async function Run(){
-    var productLink = prompt("Enter link to monitor: ");
-    if(productLink.indexOf("http") >= 0){
-        console.log("Now monitoring " + productLink);
-    }else{
-        console.log("ERROR, Invalid URL!");
+    var productLinks = prompt("Enter link to monitor (seperate by comma): ");
+   
+    var productLinksArr = productLinks.split(',');
+
+    for(var i = 0; i < productLinksArr.length; i++){
+        productLinksArr[i] = productLinksArr[i].trim();
     }
 
-    Monitor(productLink);
+var monitors = []
+
+productLinksArr.forEach(link => {
+    var p = new Promise((resolve, reject) => {
+        resolve(Monitor(link));
+    }).catch(err => console.log(err));
+
+    monitors.push(p);
+});
+
+    await Promise.allSettled(monitors);
 }
 
 Run();
